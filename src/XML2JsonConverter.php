f7c8e8af4688e254f01c9d345565a44917c80cb3
Adam Bako <bakoad@gmail.com>
2023-05-31 16:50:43 +0200

    update reg and add test

--- src/XML2JsonConverter.php
+++ src/XML2JsonConverter.php
@@ -250,7 +250,7 @@ class XML2JsonConverter
         }
 
         // I know this looks kind of funny but it changes namespaced attributes
-        if (preg_match_all('~xmlns:([a-z0-9]+)=~i', $xml, $matches)) {
+        if (preg_match_all('~xmlns:([a-z0-9\-]+)=~i', $xml, $matches)) {
             foreach (($namespaces = array_unique($matches[1])) as $namespace) {
                 $escaped_namespace = preg_quote($namespace, '~');
                 $xml = preg_replace('~[\s]xmlns:' . $escaped_namespace . '=[\'].+?[\']~i', null, $xml);
